ae7d09a021d05762954e73cb10593d58d48ee4c4
IngKevinCastillo <kevincastllo24@gmail.com>
2025-04-13 18:48:36 -0500

    soluciones de cagada de diego JAJAJAJAJ

--- UTrade Frontend/src/app/features/home/components/ventana/ventana.component.ts
+++ UTrade Frontend/src/app/features/home/components/ventana/ventana.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-ventana',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './ventana.component.css'
 })
 export class VentanaComponent {
-
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<VentanaComponent>
+  ) { }
+  salir() {
+     this.dialogRef.close();
+  }
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
 }
