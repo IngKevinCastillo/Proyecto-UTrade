4f235a77c96dff185001109bb31f81db1aa20978
IngKevinCastillo <kevincastllo24@gmail.com>
2025-04-14 13:19:19 -0500

    ya funcional lo de ver las imagenes

--- UTrade Frontend/src/app/features/home/components/anuncio-preview/anuncio-preview.component.ts
+++ UTrade Frontend/src/app/features/home/components/anuncio-preview/anuncio-preview.component.ts
@@ -13,16 +13,20 @@ export class AnuncioPreviewComponent {
   @Input() price: string = '';
   @Input() category: string = '';
   @Input() description: string = '';
-  
-  placeholderImages: number[] = [1, 2, 3, 4];
-  
-  get previewUsername(): string {
-    return this.username || 'Usuario';
-  }
-
   @Input() fotos: File[] = [];
 
+  verMas: boolean = false;
+
   get previewUrls(): string[] {
-    return this.fotos.slice(0, 4).map(file => URL.createObjectURL(file));
+    return (this.verMas ? this.fotos.slice(0, 10) : this.fotos.slice(0, 4))
+      .map(file => URL.createObjectURL(file));
+  }
+
+  toggleVerMas(): void {
+    this.verMas = !this.verMas;
+  }
+
+  get previewUsername(): string {
+    return this.username || 'Usuario';
   }
 }
