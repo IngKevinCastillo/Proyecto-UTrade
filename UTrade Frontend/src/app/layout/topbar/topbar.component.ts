fede964c31e1e56c985e14bb7029cabd9df74253
Diego Ariza <diegoandresariza54@gmail.com>
2025-04-29 13:20:06 -0500

    Menú desplegable

--- UTrade Frontend/src/app/layout/topbar/topbar.component.ts
+++ UTrade Frontend/src/app/layout/topbar/topbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-topbar',
@@ -12,7 +12,38 @@ export class TopbarComponent {
   userHandle: string = '@vendogalletas';
   avatarUrl: string = 'icons/oceana.png';
 
+  menuVisible: boolean = false;
+
   setFilter(filter: string) {
     this.activeFilter = filter;
   }
+
+  toggleMenu(event: Event) {
+    event.stopPropagation();
+    this.menuVisible = !this.menuVisible;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    if (this.menuVisible && event.target instanceof Element && !event.target.closest('.d-flex')) {
+      this.menuVisible = false;
+    }
+  }
+
+  editProfile() {
+    console.log('Editar perfil');
+  }
+
+  changePassword() {
+    console.log('Cambiar contraseña');
+  }
+
+  toggleTheme() {
+    console.log('Alternar tema');
+  }
+
+  logout() {
+    console.log('Cerrar sesión');
+  }
+
 }
