2b3012f0881c45692e86e6189b7c8c2f64b35505
IngKevinCastillo <kevincastllo24@gmail.com>
2025-05-05 21:59:06 -0500

    parte basica del panel de notificaciones lista

--- UTrade Frontend/src/app/layout/simulacionNotificaciones.ts
+++ UTrade Frontend/src/app/layout/simulacionNotificaciones.ts
@@ -3,25 +3,29 @@ export class Notificaciones {
   nombre: string;
   descripcion: string;
   tipo: string;
+  fecha: Date;
 
   constructor(
     id: number,
     nombre: string,
     descripcion: string,
-    tipo: string
+    tipo: string,
+    fecha: Date
   ) {
     this.id = id;
     this.nombre = nombre;
     this.descripcion = descripcion;
     this.tipo = tipo;
+    this.fecha = fecha;
   }
 }
 
 export const notificacionesLista: Notificaciones[] = [
-    new Notificaciones(1, "Juan Pérez", "Te ha enviado una solicitud de un producto", "solicitud"),
-    new Notificaciones(2, "Ana Gómez", "Te ha enviado un mensaje", "mensaje"),
-    new Notificaciones(3, "Carlos López", "Ha ralizado un reporte", "reporte"),
-    new Notificaciones(4, "María Rodríguez", "Te ha enviado una solicitud de un producto", "solicitud"),
-    new Notificaciones(5, "Luis Fernández", "Ha realizado una compra", "compra"),
-    new Notificaciones(6, "Laura Martínez", "Ha respondido una encuesta", "encuesta")
+    new Notificaciones(1, "Juan Pérez", "Te ha enviado una solicitud de un producto", "solicitud", new Date("2025-04-28")),
+    new Notificaciones(2, "Ana Gómez", "Te ha enviado un mensaje", "mensaje", new Date("2025-04-29")),
+    new Notificaciones(3, "Carlos López", "Ha ralizado un reporte", "reporte", new Date("2025-04-30")),
+    new Notificaciones(4, "María Rodríguez", "Te ha enviado una solicitud de un producto", "solicitud", new Date("2025-05-01")),
+    new Notificaciones(5, "Luis Fernández", "Ha realizado una compra", "compra", new Date("2025-05-02")),
+    new Notificaciones(6, "Laura Martínez", "Ha respondido una encuesta", "encuesta", new Date("2025-05-03")),
+    new Notificaciones(7, "Pedro Sánchez", "Te ha enviado un mensaje", "mensaje", new Date("2025-05-04"))
 ]
