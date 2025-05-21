cf01308074fe940f97d033b219fe30d545fd93f2
Diego Ariza <diegoandresariza54@gmail.com>
2025-05-21 16:42:56 -0500

    Editar perfil e inserción de datos casi terminada

--- UTrade Frontend/src/app/features/cuenta/mi-cuenta/mi-cuenta.component.ts
+++ UTrade Frontend/src/app/features/cuenta/mi-cuenta/mi-cuenta.component.ts
@@ -1,22 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { ConexionBackendService } from '../../../Services/conexion-backend.service';
 
 @Component({
   selector: 'app-mi-cuenta',
   templateUrl: './mi-cuenta.component.html',
   styleUrls: ['./mi-cuenta.component.css']
 })
-export class MiCuentaComponent {
-  constructor(private router: Router) {}
-  usuario = {
-    nombreVisible: 'Juan mrd',
-    nombreUsuario: 'JuanMrd',
-    correo: '******************@gmail.com',
-    telefono: '********5713'
+export class MiCuentaComponent implements OnInit {
+  usuario: any = {
+    nombreVisible: '',
+    nombreUsuario: '',
+    correo: '',
+    telefono: '',
+
   };
 
   mostrarCorreo = false;
   mostrarTelefono = false;
+
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private conexionBackend: ConexionBackendService
+  ) {}
+
+  ngOnInit(): void {
+    const usuario = JSON.parse(localStorage.getItem('usuario')!);
+    const id = usuario?.idUsuario;
+
+    if (id) {
+      const url = `${this.conexionBackend.baseUrl}/Persona/Obtener/${id}`;
+      this.http.get(url).subscribe({
+        next: (res: any) => {
+          if (res?.estado && res?.valor) {
+            const datos = res.valor;
+            this.usuario = {
+              nombreVisible: `${datos.nombres} ${datos.apellidos}`,
+              nombreUsuario: datos.nombreUsuario,
+              correo: datos.correo,
+              telefono: datos.telefono,
+              fotoPerfil: datos.fotoPerfil && datos.fotoPerfil.trim() !== ''
+              ? datos.fotoPerfil
+              : 'icons/no-photo.webp'
+            };
+          }
+        },
+        error: (err) => {
+          console.error('Error al obtener datos del usuario:', err);
+        }
+      });
+    }
+  }
 
   alternarCorreo() {
     this.mostrarCorreo = !this.mostrarCorreo;
@@ -24,6 +60,19 @@ export class MiCuentaComponent {
 
   alternarTelefono() {
     this.mostrarTelefono = !this.mostrarTelefono;
+  }
+
+  ocultarCorreo(correo: string | null): string {
+  if (!correo || correo.trim() === '') return 'No registrado';
+  const [local] = correo.split('@');
+  const oculto = '*'.repeat(local.length);
+  return `${oculto}@${correo.split('@')[1]}`;
+  }
+
+  ocultarTelefono(telefono: string | null): string {
+    if (!telefono || telefono.trim() === '') return 'No registrado';
+    const ocultos = '*'.repeat(telefono.length - 4);
+    return `${ocultos}${telefono.slice(-4)}`;
   }
 
   editarPerfil() {
