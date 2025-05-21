e1107963f6d90c51480cccd586c53c0c5aae14df
IngKevinCastillo <kevincastllo24@gmail.com>
2025-05-20 22:20:27 -0500

    no se que chingados paso con el Nodes_Module

--- UTrade Backend/API/Utilidad/Respuesta.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API.Utilidad
-{
-    public class Respuesta<T>
-    {
-        public bool estado { get; set; }
-        public T Valor { get; set; }
-        public string mgs { get; set; }
-    }
-}
