f70e96edd7bb48fb00259e1392de4f12f1e5ae59
IngKevinCastillo <kevincastllo24@gmail.com>
2025-05-22 08:53:48 -0500

    parte de perfil modificado

---
+++ UTrade Frontend/src/app/features/cuenta/mis-publicaciones/mis-publicaciones.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mis-publicaciones',
+  templateUrl: './mis-publicaciones.component.html',
+  styleUrl: './mis-publicaciones.component.css'
+})
+export class MisPublicacionesComponent {
+
+}
