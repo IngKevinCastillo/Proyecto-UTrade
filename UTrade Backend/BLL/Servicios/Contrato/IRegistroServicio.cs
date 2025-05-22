7576468220dacb28e1d52dac0165213e464fd8be
Diego Ariza <diegoandresariza54@gmail.com>
2025-05-22 14:20:28 -0500

    Verificacion backend

---
+++ UTrade Backend/BLL/Servicios/Contrato/IRegistroServicio.cs
@@ -0,0 +1,16 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Servicios.Contrato
+{
+    public interface IRegistroServicio
+    {
+        Task<string> GenerarCodigoRegistro(DateTime fechaNacimiento);
+        Task<bool> EnviarCorreoVerificacion(EnviarCorreoVerificacionDTO datos);
+        Task<bool> ValidarCodigoRegistro(string codigoRegistro, string codigoRecibido);
+    }
+}
