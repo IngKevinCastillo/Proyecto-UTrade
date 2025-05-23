3511285331d25aff352f23e523ce6ad2a96fc15f
Diego Ariza <diegoandresariza54@gmail.com>
2025-05-22 20:22:32 -0500

    Correción BLL Chat

--- UTrade Backend/BLL/Servicios/ChatServicio.cs
+++ UTrade Backend/BLL/Servicios/ChatServicio.cs
@@ -2,6 +2,7 @@
 using BLL.Servicios.Contrato;
 using DAL.Repositorios.Contrato;
 using DTO;
+using Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,10 @@ namespace BLL.Servicios
 {
     public class ChatServicio : IChatServicio
     {
-        private readonly IRepositorioGenerico<ChatDTO> _chatRepositorio;
+        private readonly IRepositorioGenerico<Chat> _chatRepositorio;
         private readonly IMapper _mapper;
 
-        public ChatServicio(IRepositorioGenerico<ChatDTO> chatRepositorio, IMapper mapper)
+        public ChatServicio(IRepositorioGenerico<Chat> chatRepositorio, IMapper mapper)
         {
             _chatRepositorio = chatRepositorio;
             _mapper = mapper;
@@ -40,7 +41,7 @@ namespace BLL.Servicios
         {
             try
             {
-                var chatCreado = await _chatRepositorio.Crear(_mapper.Map<ChatDTO>(modelo));
+                var chatCreado = await _chatRepositorio.Crear(_mapper.Map<Chat>(modelo));
                 if (chatCreado.Id == null)
                     throw new Exception("No se pudo crear");
                 return _mapper.Map<ChatDTO>(chatCreado);
